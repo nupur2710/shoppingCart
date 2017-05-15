29f74280e2bc5034f291e1159e2dc12285e05f86
Nupur <nupur@Admins-Mac-mini.local>
2017-05-15 19:32:56 +0530

    add items with form submit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 import { ShoppingListSevice } from '../shopping-list.service';
 import { Ingredient } from '../../shared/shopping.model';
 
@@ -10,11 +11,15 @@ import { Ingredient } from '../../shared/shopping.model';
 })
 export class ShoppingEditComponent implements OnInit {
 
-
+    // @ViewChild('f')
     constructor(private shoppingListService: ShoppingListSevice) {}
 
     ngOnInit() {}
 
-
+    onAddItem(form: NgForm) {
+        const value = form.value;
+        const newIngredient = new Ingredient(value.name, value.amount);
+        this.shoppingListService.addItem(newIngredient);
+    }
 
 }
