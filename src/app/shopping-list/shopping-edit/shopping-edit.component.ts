e6ba74a754063bb8e39f3f3fe7cdf5a77c5408f1
Nupur <nupur@Admins-Mac-mini.local>
2017-05-16 12:36:48 +0530

    update existing shopping list item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -41,7 +41,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         onAddItem(form: NgForm) {
         const value = form.value;
         const newIngredient = new Ingredient(value.name, value.amount);
-        this.shoppingListService.addItem(newIngredient);
+        if (this.editMode) {
+            this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+            this.editMode = false;
+        } else {
+            this.shoppingListService.addItem(newIngredient);
+        }
+
     }
 
 }
