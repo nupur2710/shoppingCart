2e0eb0c3309d6e733474ea0ebdc591b8567f9077
Nupur <nupur@Admins-Mac-mini.local>
2017-05-19 18:28:00 +0530

    add a feature module for recipies section and shared module for shared directive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuard } from './auth/auth-guard.service';
+
 
 const appRoutes: Routes = [ //localhost:4200
-    {
-        path: 'recipes',
-        component: RecipesComponent,
-        children: [{
-            path: '',
-            component: RecipeStartComponent
-        }, {
-            path: 'new',
-            component: RecipeEditComponent,
-            canActivate: [AuthGuard]
-        }, {
-            path: ':id',
-            component: RecipeDetailComponent
-        }, {
-            path: ':id/edit',
-            component: RecipeEditComponent,
-            canActivate: [AuthGuard]
-        }]
-
-    }, //localhost:4200/users
+    //localhost:4200/users
     {
         path: 'shopping-list',
         component: ShoppingListComponent,
@@ -45,10 +22,6 @@ const appRoutes: Routes = [ //localhost:4200
         path: '',
         redirectTo: '/recipes',
         pathMatch: 'full'
-    },
-    {
-        path: '**',
-        redirectTo: '/recipes'
     }
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipesModule } from './recipes/recipes.module'
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListSevice } from './shopping-list/shopping-list.service';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesService } from './recipes/recipes.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -27,24 +22,18 @@ import { AuthGuard } from './auth/auth-guard.service';
     declarations: [
         AppComponent,
         HeaderComponent,
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
         ShoppingListComponent,
         ShoppingEditComponent,
-        DropdownDirective,
-        RecipeStartComponent,
-        RecipeEditComponent,
         SignupComponent,
         SigninComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        ReactiveFormsModule,
         HttpModule,
-        AppRoutingModule
+        AppRoutingModule,
+        RecipesModule,
+        SharedModule
     ],
     providers: [ShoppingListSevice, RecipesService, DataStorageService, AuthService, AuthGuard],
     bootstrap: [AppComponent]
